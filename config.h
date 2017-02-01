216453882e8911106fbb23eaff2ef3604d30b98a
Jacob Nguyen <jnguyen@hmc.edu>
2017-01-31 19:54:48 -0800

    First step (pins.h and config.h initial modification)

--- config.h
+++ config.h
@@ -2,10 +2,13 @@
 
 #define NUM_INPUTS 3
 #define NUM_OUTPUTS 16
-#define NUM_POWERS 10
-#define NUM_GROUNDS 11
+#define NUM_POWERS 3
+#define NUM_GROUNDS 3
 
-const uint8_t powers[] = {P1, P5, P7, P11, P13, P15, P17, P34, P37, P39};
-const uint8_t grounds[] = {P2, P4, P6, P10, P12, P14, P16, P35, P36, P38, P40};
-const uint8_t inputs[] = {P8, P9, P3}; // clk1, clk2, reset
-const uint8_t outputs[] = {P18, P19, P20, P21, P22, P23, P24, P25, P26, P27, P28, P29, P30, P31, P32, P33}; // anodes, then cathodes
+const uint8_t powers[] = {P5, P37, P39};
+const uint8_t grounds[] = {P4, P36, P40};
+const uint8_t inputs[] = {P3. P6,P7,P8,P9, P10,P11,P12,P13, // reset, col<3:0>, row<3:0?
+                          P14, P15, P16, P17, P18}; , // direction, player, read, ph1, ph2
+const uint8_t outputs[] = {P19, // data_ready
+                           P20,P21,P22,P23,P24,P25,P26,P27,P28,P29,P30,P31}; // data_out<0:11>
+const uint8_t unused[] = [P1,P2, P32,P33,P34,P35, P38];
--- pins.h
+++ pins.h
@@ -1,41 +1,63 @@
-//#define P1 23
-//#define P2 22
-#define P3 23
-//#define P4 20
-//#define P5 19
-//#define P6 18
-//#define P7 17
-#define P8 22
-#define P9 21
-//#define P10 14
-//#define P11 13
-//#define P12 51
-//#define P13 53
-//#define P14 52
-//#define P15 33 /// I accidentally cut the trace on my board!
-//#define P16 38
-//#define P17 37
-#define P18 20
-#define P19 19
-#define P20 18
+#define P1 1 // Unused
+#define P2 2 // Unused
 
-#define P21 17
-#define P22 16
-#define P23 15
-#define P24 14
-#define P25 13
-#define P26 7
-#define P27 6
-#define P28 5
-#define P29 4
-#define P30 3
-#define P31 2
-#define P32 1
-#define P33 0
-//#define P34 8
-//#define P35 7
-//#define P36 6
-//#define P37 5
-//#define P38 4
-//#define P39 3
-//#define P40 2
+//---Inputs---//
+#define P3 3 // reset
+//---Inputs---//
+
+//---Input/Output---//
+#define P4 4 // gnd
+#define P5 5 // vdd
+//---Input/Output---//
+
+//---Inputs---//
+#define P6 6 // col<3>
+#define P7 7 // col<2>
+#define P8 8 // col<1>
+#define P9 9 // col<0>
+
+#define P10 10 // row<3>
+#define P11 11 // row<2>
+#define P12 12 // row<1>
+#define P13 13 // row<0>
+
+#define P14 14 // direction
+#define P15 15 // player
+#define P16 16 // read
+#define P17 17 // ph1
+#define P18 18 // ph2
+//---Inputs---//
+
+//---Outputs---//
+#define P19 19 // data_ready
+
+#define P20 20 // data_out<0>
+#define P21 21 // data_out<1>
+#define P22 22 // data_out<2>
+#define P23 23 // data_out<3>
+#define P24 24 // data_out<4>
+#define P25 25 // data_out<5>
+#define P26 26 // data_out<6>
+#define P27 27 // data_out<7>
+#define P28 28 // data_out<8>
+#define P29 29 // data_out<9>
+#define P30 30 // data_out<10>
+#define P31 31 // data_out<11>
+//---Outputs---//
+
+#define P32 32 // Unused
+#define P33 33 // Unused
+#define P34 34 // Unused
+#define P35 35 // Unused
+
+//---Input/Output---//
+#define P36 36 // gnd
+#define P37 37 // vdd
+//---Input/Output---//
+
+#define P38 38 // Unused
+
+//---Input/Output---//
+#define P39 39 // vdd
+#define P40 40 // gnd
+//---Input/Output---//
